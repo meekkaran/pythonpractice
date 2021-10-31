c482183f9309534d9d96b61c5258d9ba797ad709
Meek Karan <meekkaran@gmail.com>
2021-10-31 14:44:14 +0300

    lists

---
+++ reverselist.py
@@ -0,0 +1,6 @@
+# string ="Hello"
+# print (string[::-1])
+
+list =[1,2,3,4,5]
+list.reverse()
+print (list)
---
+++ sumOfConsecutiveNo.py
@@ -0,0 +1,5 @@
+
+N = int(input())
+for N in range(1, 101):
+    N += 100
+    print (N)
